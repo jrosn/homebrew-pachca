3e1c369de7217fc9194a91fc989202c0f50fc6ac
Roman Sosnovsky <rvsosnovsky@gmail.com>
2025-08-03 11:59:29 -0700

    Updated download link

--- Casks/pachca.rb
+++ Casks/pachca.rb
@@ -1,10 +1,9 @@
 cask "pachca" do
   arch arm: "aarch64"
 
-  version "2.5.0"
   sha256 "1d3f76baed87166344d2eedc5eaa7a76d198d08986db27d920f6627af509e90d"
 
-  url "https://install.pachca.com/Pachca%20#{version}-arm64.dmg"
+  url "https://install.pachca.com/mac/dmg/arm64"
   name "Pachca"
   desc "Corporate messenger for modern teams"
   homepage "https://www.pachca.com/"
@@ -12,7 +11,7 @@ cask "pachca" do
   # Documentation: https://docs.brew.sh/Brew-Livecheck
   livecheck do
     url "https://www.pachca.com/apps/"
-    regex(/href=.*Pachca%20(\d+\.\d+\.\d+)-arm64\.dmg/i)
+    regex(/href=.*arm64.*.dmg/i)
   end
 
   auto_updates true
